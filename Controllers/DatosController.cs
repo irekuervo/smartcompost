eae1ed0041f92e788c059e2cb2aeebebc132e71d
erik <erik.schatz@gmail.com>
2023-09-26 23:30:33 -0300

    init

---
+++ Controllers/DatosController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CreativeTim.Argon.DotNetCore.Free.Controllers
+{
+    [Route("api/datos")]
+    [ApiController]
+    public class DatosController : Controller
+    {
+
+        static List<Dato> datosNodo = new List<Dato>();
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var random = new Random();
+
+            datosNodo.Add(new Dato { Id = datosNodo.Count, Valor = random.Next(1, 100) });
+
+            return Ok(datosNodo);
+        }
+    }
+
+    public class Dato
+    {
+        public int Id { get; set; }
+        public int Valor { get; set; }
+    }
+}
