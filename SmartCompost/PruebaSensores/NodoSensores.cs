5ef1b350e2ab074830266fcadb3639363f0e64e2
eschatz <eschatz@ispectrum.com.ar>
2024-08-10 13:57:42 -0300

    fix cuentas

--- SmartCompost/PruebaSensores/NodoSensores.cs
+++ SmartCompost/PruebaSensores/NodoSensores.cs
@@ -1,11 +1,12 @@
 using Iot.Device.Ds18b20;
 using nanoFramework.Device.OneWire;
-using NanoKernel.Nodos;
-using System.Threading;
-using System;
 using nanoFramework.Hardware.Esp32;
-using System.Device.Adc;
 using NanoKernel.Dominio;
+using NanoKernel.Hilos;
+using NanoKernel.Nodos;
+using System;
+using System.Device.Adc;
+using System.Threading;
 
 namespace NodoAP
 {
@@ -41,39 +42,42 @@ namespace NodoAP
              */
             adc = new AdcController();
 
-            /* HUMEDAD          -----> ADC Channel 9 - GPIO 39 */
-            humedadAdc = adc.OpenChannel(9);
+            /* HUMEDAD          -----> ADC Channel 4 - GPIO 32 */
+            humedadAdc = adc.OpenChannel(4);
 
             /* BATERIA SENSOR   -----> ADC Channel 6 - GPIO 34 */
             bateriaAdcSensor = adc.OpenChannel(6);
 
             /* BATERIA AP       -----> ADC Channel 7 - GPIO 35 */
-            bateriaAdcAp = adc.OpenChannel(7);
+            // bateriaAdcAp = adc.OpenChannel(7);
 
             ds18b20.IsAlarmSearchCommandEnabled = false;
-            /*if (ds18b20.Initialize())
-            {
-                Console.WriteLine($"Is sensor parasite powered?:{ds18b20.IsParasitePowered}");
-                string devAddrStr = "";
-                foreach (var addrByte in ds18b20.Address)
-                {
-                    devAddrStr += addrByte.ToString("X2");
-                }
 
-                Console.WriteLine($"Sensor address:{devAddrStr}");
+            //Hilo.Intentar(() => {
+            //    if (ds18b20.Initialize())
+            //    {
+            //        Console.WriteLine($"Is sensor parasite powered?:{ds18b20.IsParasitePowered}");
+            //        string devAddrStr = "";
+            //        foreach (var addrByte in ds18b20.Address)
+            //        {
+            //            devAddrStr += addrByte.ToString("X2");
+            //        }
 
-            }
-            else {
-                throw new Exception("No anduvo viejo");
-            }
-            */
+            //        Console.WriteLine($"Sensor address:{devAddrStr}");
+
+            //    }
+            //    else
+            //    {
+            //        throw new Exception("No anduvo viejo");
+            //    }
+            //});
         }
 
         public override void Loop(ref bool activo)
         {
             temperatura = MedirTemperatura();
             humedad = MedirHumedad();
-            bateriaAp = MedirBateriaAp();
+            //bateriaAp = MedirBateriaAp();
             bateriaSensor = MedirBateriaSensor();
 
             Thread.Sleep(1000);
@@ -90,12 +94,12 @@ namespace NodoAP
             */
             int analogValue = humedadAdc.ReadValue();
 
-            int vSensor = (analogValue / 1023) * 5;
+            float vSensor = (analogValue / 4095f * 3.3f);
             double humidityPercentage = (-5.9732 * vSensor * vSensor * vSensor) + (63.948 * vSensor * vSensor) - 232.8 * vSensor + 308.98;
 
             //Ver la funcion Pow
             //double humidityPercentage = (-5.9732*Pow(vSensor,3)) + (63.948*Pow(vSensor,2)) - 232.8*vSensor + 308.98;
-            
+
             Console.WriteLine($"Humedad: {humidityPercentage}");
             return analogValue;
 
@@ -104,7 +108,7 @@ namespace NodoAP
         private int MedirBateriaSensor()
         {
             int analogValue = bateriaAdcSensor.ReadValue();
-            int vSensor = (analogValue / 1023) * 5;
+            float vSensor = analogValue / 4095f * 3.3f;
             double bateriaPorcentaje = 90.9 * vSensor - 354.5;
 
             /*definir la matematica para devilver porcentaje*/
@@ -115,7 +119,7 @@ namespace NodoAP
         private int MedirBateriaAp()
         {
             int analogValue = bateriaAdcAp.ReadValue();
-            int vSensor = (analogValue / 1023) * 5;
+            float vSensor = analogValue / 4095f * 3.3f;
             double bateriaPorcentaje = 100 * vSensor - 400;
 
 
